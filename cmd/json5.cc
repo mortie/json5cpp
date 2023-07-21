5e58d4b86c18af229cc90143b6a35ea9f96c467e
Martin Dørum <martid0311@gmail.com>
2023-07-21 11:41:45 +0200

    json5 command changes

--- cmd/json5.cc
+++ cmd/json5.cc
@@ -4,19 +4,32 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
+	if (argc <= 1) {
+		Json::Value val;
+		if (!Json5::parse(std::cin, val)) {
+			std::cerr << "Could not parse stdin\n";
+			return 1;
+		}
+
+		std::cout << val << '\n';
+		return 0;
+	}
+
 	for (int i = 1; i < argc; ++i) {
 		Json::Value val;
 		std::ifstream is{argv[i]};
 		if (!is) {
 			std::cerr << "Could not open " << argv[i] << '\n';
-			continue;
+			return 1;
 		}
 
 		if (!Json5::parse(is, val)) {
 			std::cerr << "Could not parse " << argv[i] << '\n';
-			continue;
+			return 1;
 		}
 
 		std::cout << val << '\n';
 	}
+
+	return 0;
 }
