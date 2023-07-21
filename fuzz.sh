d6f2d1e410e4a325981aea0e41f89da98ae01792
Martin Dørum <martid0311@gmail.com>
2023-07-21 22:03:01 +0200

    fix fuzzing

--- fuzz.sh
+++ fuzz.sh
@@ -7,13 +7,17 @@ mkdir -p fuzz-data
 make CXX=afl-c++ DESTDIR=./fuzz-data
 
 mkdir -p fuzz-data/seeds
+echo '100' > fuzz-data/seeds/number.json
+echo '"Hello World"' > fuzz-data/seeds/string.json
+echo '[10, 20]' > fuzz-data/seeds/array.json
+echo '{foo: "bar"}' > fuzz-data/seeds/object.json
 mkdir -p fuzz-data/output
 
 cd fuzz-data
 
-afl-fuzz -o output -M "json5-fuzzer-1" -- ./json5 &
+afl-fuzz -i seeds -o output -M "json5-fuzzer-1" -- ./json5 &
 for i in $(seq 2 20); do
-	afl-fuzz -o output -S "json5-fuzzer-$i" -- ./json5 >/dev/null &
+	afl-fuzz -i seeds -o output -S "json5-fuzzer-$i" -- ./json5 >/dev/null &
 done
 
 wait
