70179a82b924a74b6aa598e1ba304a59547bf7a7
Martin Dørum <martid0311@gmail.com>
2024-11-29 16:18:28 +0100

    support separate compilation

---
+++ examples/separate-compilation/json5cpp.h
@@ -0,0 +1,4 @@
+// This file exists to re-export json5cpp with JSON5CPP_FWD_ONLY,
+// so that other source files don't need to remember to do that.
+#define JSON5CPP_FWD_ONLY
+#include "../../json5cpp.h" // IWYU pragma: keep
